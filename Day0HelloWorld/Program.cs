03d456c5c4234eed864cc8bd77f92c86597bde75
mstfEmk <mstfEmk@gmail.com>
2018-10-17 16:48:59 +0300

    Day0HelloWorld

--- Day0HelloWorld/Program.cs
+++ Day0HelloWorld/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Day0HelloWorld
 {
-    class Program
+    class Solution
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+
+            Console.WriteLine(inputString);
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
         }
     }
 }
