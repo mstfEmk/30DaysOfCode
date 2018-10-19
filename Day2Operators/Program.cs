458b0f4afeb0796685e59f8b043e467c1d3bad32
mstfEmk <mstfEmk@gmail.com>
2018-10-19 17:10:31 +0300

    Day2Operators

---
+++ Day2Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double mealCost = Double.Parse(Console.ReadLine());
+            int tipPercent = Int32.Parse(Console.ReadLine());
+            int taxPercent = Int32.Parse(Console.ReadLine());
+
+            int totalCost = Solve(mealCost, tipPercent, taxPercent);
+
+            Console.WriteLine(totalCost);
+        }
+
+        private static int Solve(double mealCost, int tipPercent, int taxPercent)
+        {
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
+            double totalCost = mealCost + tip + tax;
+            return Convert.ToInt32(totalCost);
+            
+        }
+    }
+}
