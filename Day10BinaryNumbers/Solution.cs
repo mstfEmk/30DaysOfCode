7e834dd93bf0be996113e34861777f0c7cb2ce32
mstfEmk <mstfEmk@gmail.com>
2018-10-27 10:32:03 +0300

    Day10BinaryNumbers

---
+++ Day10BinaryNumbers/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10BinaryNumbers
+{
+    class Solution
+    {
+        static int FindConsecutiveOnes(string binary)
+        {
+            int count = 0;
+            int max = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    count++;
+                    if (count > max)
+                    {
+                        max = count;
+                    }
+                } else
+                {
+                    count = 0;
+                }
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+            int count = FindConsecutiveOnes(binary);
+            Console.WriteLine(count);
+
+        }
+    }
+}
