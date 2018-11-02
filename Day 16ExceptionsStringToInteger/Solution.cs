3ee0998ccfe435ac74190cc144bcb7f6bd7ad30a
mstfEmk <mstfEmk@gmail.com>
2018-11-02 07:27:53 +0300

    Day 16ExceptionsStringToInteger

---
+++ Day 16ExceptionsStringToInteger/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16ExceptionsStringToInteger
+{
+    class Solution
+    {
+        
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            try
+            {
+                int number = int.Parse(s); 
+                Console.WriteLine(number);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
