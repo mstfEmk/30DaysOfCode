8bf5e47aea449d3c6e4b3d6d89ca90592f985ad6
mstfEmk <mstfEmk@gmail.com>
2018-11-09 08:24:30 +0300

    Day23BSTLevel-OrderTraversal

--- Day23BSTLevel-OrderTraversal/Solution.cs
+++ Day23BSTLevel-OrderTraversal/Solution.cs
@@ -4,53 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day22BinarySearchTrees
+namespace Day23BSTLevel_OrderTraversal
 {
     class Node
     {
-
         public Node left, right;
         public int data;
-
         public Node(int data)
         {
             this.data = data;
             left = right = null;
         }
-
     }
-
     class Solution
     {
-        /* Adamlar dal saymak yerine yine node sayıp ilki için 0 yerine -1 dönüyorlar.
-        if(root == null)
-            return -1;
-        return Math.Max(1+ getHeight(root.left),1+getHeight(root.right));
-        */
 
-        static int getHeight(Node root)
+        static void levelOrder(Node root)
         {
-            if ((root == null) || (root.left == null && root.right == null))
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            
+            while (queue.Count > 0)
             {
-                return 0;
+                Node node = queue.Dequeue();
+                Console.Write(node.data + " ");
+                if (node.left != null)
+                    queue.Enqueue(node.left);
+                if (node.right != null)
+                    queue.Enqueue(node.right);
             }
-
-            int leftHeight = 0;
-            int rightHeight = 0;
-
-            if (root.left != null)
-            {
-                leftHeight = getHeight(root.left);
-            }
-            if (root.right != null)
-            {
-                rightHeight = getHeight(root.right);
-            }
-
-            return Math.Max(leftHeight, rightHeight) + 1;
-
         }
-
         static Node insert(Node root, int data)
         {
             if (root == null)
@@ -73,28 +56,16 @@ namespace Day22BinarySearchTrees
                 return root;
             }
         }
-
         static void Main(String[] args)
         {
             Node root = null;
             int T = Int32.Parse(Console.ReadLine());
-            // int[] arr = { 3, 5, 2, 1, 4, 6, 7 };
-            //            { 20, 50, 35, 44, 9, 15, 62, 11, 13 };
-            // { 3, 5, 2, 1, 4, 6, 7 };
-            // { 20, 50, 35, 44, 9, 15, 62, 11, 13 };
-
-            // int T = arr.Length;
-
-            // for (int i = 0; i < T; i++)
             while (T-- > 0)
             {
                 int data = Int32.Parse(Console.ReadLine());
                 root = insert(root, data);
-                // root = insert(root, arr[i]);
             }
-            root = null;
-            int height = getHeight(root);
-            Console.WriteLine(height);
+            levelOrder(root);
 
         }
     }
