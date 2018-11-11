b803c14e86e3d2de61803e9c05c08274f4cc2dca
mstfEmk <mstfEmk@gmail.com>
2018-11-11 17:22:09 +0300

    Day25RunningTimeAndComplexity

---
+++ Day25RunningTimeAndComplexity/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25RunningTimeAndComplexity
+{
+    class Solution
+    {
+        static bool IsPrime(int number)
+        {
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            while (T-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string isPrime = (n >=2 && IsPrime(n)) ? "Prime" : "Not prime";
+                Console.WriteLine(isPrime);
+            }
+        }
+    }
+}
